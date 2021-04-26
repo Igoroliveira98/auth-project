2e091e2f81db5253ab3b563f8aa836e5c06eca07
Igor Oliveira <igoroliveira2798@gmail.com>
2021-04-26 19:21:03 -0300

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,18 @@
-const express = require("express")
-const routes = require("./routes");
+// This file starter a new server with express
+
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+const routes = require("./routes");
+const Error = require("./middlewares/Error");
+
 
 app.use(express.json());
+app.use(cors());
+
 app.use(routes);
+app.use(Error)
 
 
 app.listen(3333, () => console.log("Server is running!! 🔥🔥"));
