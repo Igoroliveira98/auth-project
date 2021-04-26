79e9e3dac292dba3b6ffe9a43818db5db7280e46
Igor Oliveira <igoroliveira2798@gmail.com>
2021-04-26 19:21:19 -0300

    Update routes.js

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,5 +1,9 @@
 const routes = require("express").Router();
 
+const UserController = require("./controllers/UserController");
+
+routes.get("/index", UserController.index);
+routes.post("/create", UserController.create);
 
 
 module.exports = routes;
