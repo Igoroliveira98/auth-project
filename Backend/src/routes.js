5fe58979101ca643fab7c6d4dbd21983e98ec7f3
Igor Oliveira <igoroliveira2798@gmail.com>
2021-04-27 16:36:00 -0300

    update routes

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,9 +1,20 @@
 const routes = require("express").Router();
 
 const UserController = require("./controllers/UserController");
+const LoginController = require("./controllers/LoginController");
+const SessionController = require("./controllers/SessionController");
+
+// User route:
 
 routes.get("/index", UserController.index);
 routes.post("/create", UserController.create);
 
+// Login route:
+
+routes.post("/login", LoginController.Login);
+
+// Session route:
+
+routes.get("/session", SessionController.Session)
 
 module.exports = routes;
